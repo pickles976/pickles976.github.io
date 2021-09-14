fa2526842a3ca7312bcdbe6a3909405505a91e28
Juan Gomez <sebaslogo@gmail.com>
2021-09-13 21:26:09 -0500

    removed some js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -54,5 +54,4 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 document.getElementById("wrapper").style.height = document.height;
-document.getElementById("back").style.height = document.height;
 document.getElementById("boids").style.height = document.height;
